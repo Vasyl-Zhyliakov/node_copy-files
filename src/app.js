b0dde3176333d0ffb35c1af6350301153396c2d3
Vasyl Zhyliakov <snak87@gmail.com>
2025-05-29 16:10:40 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const fs = require('fs');
+
+function copy() {
+  const [fileName, fileCopy] = process.argv.slice(2);
+
+  if (!fileName || !fileCopy) {
+    console.error('Need more arguments');
+  }
+
+  if (fileName === fileCopy) {
+    console.error('Source and destination are the same');
+  }
+
+  let data;
+
+  try {
+    data = fs.readFileSync(fileName, 'utf8');
+  } catch (err) {
+    console.error(err);
+  }
+
+  try {
+    fs.writeFileSync(fileCopy, data);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copy();
+
+module.exports = { copy };
